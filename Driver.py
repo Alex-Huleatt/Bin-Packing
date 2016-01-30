e22af8db376fc8fd62c51fce2df1245df578dc6a
Alex.Huleatt <you@example.com>
2016-01-30 11:34:46 -0700

    Various

--- Driver.py
+++ Driver.py
@@ -146,7 +146,7 @@ def get_dataset(num):
      #generate 1,000 rectangles
      #widths in the range [1,500]
      #heights in the range [1,500]
-    sizes = rect_gen.randomSplit(10000,500,500)
+    sizes = rect_gen.randomSplit(1000,500,500)
     maxTime = 5 #just a constant time I guess.
     return (sizes,maxTime)
 
