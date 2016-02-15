cd3b415c81b663edad9012dcc3bf01cef6991f8b
Alex.Huleatt <you@example.com>
2016-02-15 00:22:39 -0700

    lol formatting

--- rect_collision.py
+++ rect_collision.py
@@ -1,17 +1,17 @@
 import statistics
 
 def get_overlap(sizes, posns):
-         global inter_count
-         inter_count=0
-         if (len(sizes) != len(posns)):
-                  raise ValueError('Length mismatch.')
-         if (len(sizes) == 0):
-                  return None
-         rt = []
-         for i in range(len(sizes)):
-                  rt.append( (posns[i],sizes[i]) )
-         
-         return grid_detect(rt)
+    global inter_count
+    inter_count=0
+    if (len(sizes) != len(posns)):
+        raise ValueError('Length mismatch.')
+    if (len(sizes) == 0):
+        return None
+    rt = []
+    for i in range(len(sizes)):
+        rt.append( (posns[i],sizes[i]) )
+    
+    return grid_detect(rt)
 
 '''
 This is a pretty neat rectangle collision detection function.
@@ -22,44 +22,44 @@ Then we do the naive overlap check on any rectangles which share a cell.
 There are numerous performance gains that could me made, but were entirely unnecessary for this.
 '''
 def grid_detect(rt):
-         avg_w = statistics.median([s[1][0] for s in rt])
-         avg_h = statistics.median([s[1][1] for s in rt])
-         merp={}
-         for r in rt: # n
-                  coll = place_rect(r,avg_w/3,avg_h/3, merp)
-                  if coll is not None:
-                           print(inter_count)
-                           return coll
-         print(inter_count)
-         return None
+    avg_w = statistics.median([s[1][0] for s in rt])
+    avg_h = statistics.median([s[1][1] for s in rt])
+    merp={}
+    for r in rt: # n
+        coll = place_rect(r,avg_w/3,avg_h/3, merp)
+        if coll is not None:
+            print(inter_count)
+            return coll
+    print(inter_count)
+    return None
 
 def place_rect(r, dx, dy, merp):
-         tx = r[0][0]//dx
-         ty = r[0][1]//dy
-         i = tx
-         while i * dx <= r[0][0] + r[1][0]:
-                  j = ty
-                  while j * dy <= r[0][1] + r[1][1]:
-                           p = (i,j)
-                           if (p in merp): #usually this shouldn't get hit, hopefully.
-                                    for t in merp[p]: #for each rect in this cell, compare.
-                                             if intersecting(r, t):
-                                                      return (r,t)
-                                    merp[p].append(r)
-                           else:
-                                    merp[p]=[r]
-                           j += 1
-                  i += 1
-         return None
+    tx = r[0][0]//dx
+    ty = r[0][1]//dy
+    i = tx
+    while i * dx <= r[0][0] + r[1][0]:
+        j = ty
+        while j * dy <= r[0][1] + r[1][1]:
+            p = (i,j)
+            if (p in merp): #usually this shouldn't get hit, hopefully.
+                for t in merp[p]: #for each rect in this cell, compare.
+                    if intersecting(r, t):
+                        return (r,t)
+                merp[p].append(r)
+            else:
+                merp[p]=[r]
+            j += 1
+        i += 1
+    return None
 #ew
 inter_count = 0
 def intersecting(r1, r2):
-         global inter_count
-         inter_count+=1
-         #print(r1,r2)
-         return not (
-                   r1[0][0] >= r2[0][0] + r2[1][0]
-           or r2[0][0] >= r1[0][0] + r1[1][0]
-           or r1[0][1] >= r2[0][1] + r2[1][1] 
-           or r2[0][1] >= r1[0][1] + r1[1][1])
+    global inter_count
+    inter_count+=1
+    #print(r1,r2)
+    return not (
+         r1[0][0] >= r2[0][0] + r2[1][0]
+      or r2[0][0] >= r1[0][0] + r1[1][0]
+      or r1[0][1] >= r2[0][1] + r2[1][1] 
+      or r2[0][1] >= r1[0][1] + r1[1][1])
 
--- rect_gen.py
+++ rect_gen.py
@@ -3,39 +3,45 @@ import random
 
 '''let's have a method that splits a big rectangle into a list of rectangles recursively'''
 def perfectSplit(width,height,maxWidth,maxHeight):
-         if (width < 1 or height < 1):
-                  return []
-         (new_width,new_height) = (random.randint(1,min(width,maxWidth)),random.randint(1,min(height,maxHeight)))
+    if (width < 1 or height < 1):
+        return []
+    (new_width,new_height) = (random.randint(1,min(width,maxWidth)),random.randint(1,min(height,maxHeight)))
 
-         #all rectangles are at (0,0), the issue will be solved by caller function
-         thisRect = (new_width,new_height)
-         myList = [thisRect]
-         toRight = perfectSplit(width-new_width,height,maxWidth,maxHeight)
-         if (len(toRight) > 0):
-                  myList.extend(toRight)
+    #all rectangles are at (0,0), the issue will be solved by caller function
+    thisRect = (new_width,new_height)
+    myList = [thisRect]
+    toRight = perfectSplit(width-new_width,height,maxWidth,maxHeight)
+    if (len(toRight) > 0):
+        myList.extend(toRight)
 
-         below = perfectSplit(new_width, height-new_height,maxWidth,maxHeight)
-         if (len(below) > 0):
-                  myList.extend(below)
-         return myList
+    below = perfectSplit(new_width, height-new_height,maxWidth,maxHeight)
+    if (len(below) > 0):
+        myList.extend(below)
+    return myList
 
 '''randomly produces specified number of rectangles'''
 def randomSplit(count, maxWidth, maxHeight):
-         lst = []
-         for i in range(count):
-                  lst.append( (random.randint(1,maxWidth), random.randint(1,maxHeight)) )
-         return lst
+    lst = []
+    for i in range(count):
+        lst.append( (random.randint(1,maxWidth), random.randint(1,maxHeight)) )
+    return lst
 
 def highVariance(count, maxWidth, maxHeight):
-         lst = []
-         for i in range(count):
+    lst = []
+    for i in range(count):
 
-                  lst.append( (varianceInt(1,maxWidth), varianceInt(1,maxHeight)) )
-         return lst
+        lst.append( (varianceInt(1,maxWidth), varianceInt(1,maxHeight)) )
+    return lst
 
 def varianceInt(lo, hi):
-         start = random.randint(lo,hi)
-         highest = hi**(hi**.2)
-         start = start**(start**.2)
-         start = (start - lo)/(highest-lo)
-         return start * (hi-lo)
+    start = random.randint(lo,hi)
+    highest = hi**(hi**.2)
+    start = start**(start**.2)
+    start = (start - lo)/(highest-lo)
+    return start * (hi-lo)
+
+
+
+
+
+
